d19310e1965eef0c4728cf9ab5509c4a5ea0f2bd
Tay Jing Xuan <tayjxn@gmail.com>
2017-06-20 16:39:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -144,10 +144,6 @@ app.post('/submit', urlencodedParser, function(req, res){
 })**/
 
 app.get('/data', function(req, res){
-  var data=[[{"sampleID":1,"A":"Coke","B":"Pepsi"},{"sampleID":2,"A":"Coke","B":"Pepsi"},{"sampleID":3,"A":"Coke","B":"Pepsi"},{"sampleID":4,"A":"Pepsi","B":"Coke"},{"sampleID":5,"A":"Pepsi","B":"Coke"},{"sampleID":6,"A":"Coke","B":"Pepsi"},{"sampleID":7,"A":"Coke","B":"Pepsi"},{"sampleID":8,"A":"Coke","B":"Pepsi"},{"sampleID":9,"A":"Coke","B":"Pepsi"},{"sampleID":10,"A":"Coke","B":"Pepsi"},{"sampleID":11,"A":"Coke","B":"Pepsi"},{"sampleID":12,"A":"Coke","B":"Pepsi"},{"sampleID":13,"A":"Pepsi","B":"Coke"},{"sampleID":14,"A":"Coke","B":"Pepsi"},{"sampleID":15,"A":"Pepsi","B":"Coke"},{"sampleID":16,"A":"Coke","B":"Pepsi"},{"sampleID":17,"A":"Coke","B":"Pepsi"},{"sampleID":18,"A":"Coke","B":"Pepsi"},{"sampleID":19,"A":"Pepsi","B":"Coke"},{"sampleID":20,"A":"Pepsi","B":"Coke"},{"sampleID":21,"A":"Coke","B":"Pepsi"},{"sampleID":22,"A":"Pepsi","B":"Coke"},{"sampleID":23,"A":"Coke","B":"Pepsi"},{"sampleID":24,"A":"Coke","B":"Pepsi"},{"sampleID":25,"A":"Coke","B":"Pepsi"},{"sampleID":26,"A":"Coke","B":"Pepsi"},{"sampleID":27,"A":"Pepsi","B":"Coke"},{"sampleID":28,"A":"Coke","B":"Pepsi"},{"sampleID":29,"A":"Pepsi","B":"Coke"},{"sampleID":30,"A":"Coke","B":"Pepsi"}],[{"studentID":"h1710011","sampleID":17,"drink1":"Pepsi","drink2":"A"},{"studentID":"h1710013","sampleID":4,"drink1":"Pepsi","drink2":"B"},{"studentID":"h1710016","sampleID":15,"drink1":"Coke","drink2":"A"},{"studentID":"h1710022","sampleID":6,"drink1":"Coke","drink2":"B"},{"studentID":"h1710028","sampleID":8,"drink1":"Coke","drink2":"A"},{"studentID":"h1710044","sampleID":3,"drink1":"Pepsi","drink2":"A"},{"studentID":"h1710047","sampleID":18,"drink1":"Pepsi","drink2":"B"},{"studentID":"h1710051","sampleID":13,"drink1":"Pepsi","drink2":"A"},{"studentID":"H1710056","sampleID":14,"drink1":"Coke","drink2":"A"},{"studentID":"h1710076","sampleID":25,"drink1":"Coke","drink2":"A"},{"studentID":"h1710096","sampleID":21,"drink1":"Coke","drink2":"B"},{"studentID":"h1710100","sampleID":5,"drink1":"Coke","drink2":"A"},{"studentID":"h1710103","sampleID":7,"drink1":"Coke","drink2":"B"},{"studentID":"h1710109","sampleID":16,"drink1":"Pepsi","drink2":"B"},{"studentID":"h1710111","sampleID":10,"drink1":"Pepsi","drink2":"A"},{"studentID":"h1710115","sampleID":20,"drink1":"Coke","drink2":"A"},{"studentID":"h1710116","sampleID":22,"drink1":"Coke","drink2":"A"},{"studentID":"h1710119","sampleID":24,"drink1":"Pepsi","drink2":"A"},{"studentID":"h1710120","sampleID":19,"drink1":"Coke","drink2":"B"},{"studentID":"h1710131","sampleID":9,"drink1":"Coke","drink2":"A"},{"studentID":"h1710149","sampleID":23,"drink1":"Coke","drink2":"A"}]];
-  res.json(data);
-})
-/**app.get('/data', function(req, res){
 	var data = [];
 	con.query('SELECT *FROM options', function(err, rows){
 		if(err) throw err;
@@ -162,7 +158,7 @@ app.get('/data', function(req, res){
 		console.log('data is complete');
 		res.json(data);
 	})
-})**/
+})
 
 app.get('/showData', function(req,res){
   res.sendFile(__dirname+'/'+'Data.html');
